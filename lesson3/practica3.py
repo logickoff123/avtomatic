6b7bd51626f8b7e7e40358f9bec0ed003c28be80
Sonya Ustinova <SonyaUstinova@gmail.com>
2023-09-07 00:41:20 +0300

    практика с крнем

---
+++ lesson3/practica3.py
@@ -0,0 +1,24 @@
+#def my_personal_sun(x: int |float , y:int) -> int|float: # последня херь со стрелочкой это то что функция вернет
+    #pass
+
+#print('hello')
+    
+#def my_personal_sum (*args, **kwargs) -> int|float
+   # print (f'Args: {args}')
+   
+   
+#практика 3 (пишем функцию)
+n = int(input())
+if n==1:
+    print(1)
+def guesss(n):
+    for i in range (n):
+        if i*i == n:
+            perem = i
+        i+=1
+    return (perem)
+
+q = guesss(n)
+print (q)
+    
+ 
