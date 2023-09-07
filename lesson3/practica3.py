0a3d4f4a7a664cafd2d36648afa08412e5d55afb
Sonya Ustinova <SonyaUstinova@gmail.com>
2023-09-07 09:20:29 +0300

    jngecnbnt

--- lesson3/practica3.py
+++ lesson3/practica3.py
@@ -8,17 +8,27 @@
    
    
 #практика 3 (пишем функцию)
-n = int(input())
-if n==1:
-    print(1)
-def guesss(n):
-    for i in range (n):
-        if i*i == n:
-            perem = i
-        i+=1
-    return (perem)
 
-q = guesss(n)
-print (q)
+n = int(input())
+
+def guess(n):
+    if n/2<1:
+        p = (n)
+    elif n>0:
+        for i in range(n+1):
+            if i*i == n:
+                p = i
+                i= i+1
+                if p is not None:
+                    return p
+            elif i == n:
+                    p = "Я не могу такое вычетать"
+                    
+    return p
+
+g = guess(n)
+print (g)
+
+
     
  
